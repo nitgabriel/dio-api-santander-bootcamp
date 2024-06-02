6d493927690fe750cefe720e3a8337b124c38436
nitgabriel <gabrield114@gmail.com>
2024-06-01 23:18:04 -0300

    Implementação da camada de domínio.

---
+++ src/main/java/br/com/dio/domain/model/Feature.java
@@ -0,0 +1,6 @@
+package br.com.dio.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {}
---
+++ src/main/java/br/com/dio/domain/model/News.java
@@ -0,0 +1,6 @@
+package br.com.dio.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {}
